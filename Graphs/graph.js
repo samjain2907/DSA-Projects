d879178511d872df1167af2c2deb97b8929ee2d6
Samarth Jain <jain.samarth2907@gmail.com>
2020-04-13 13:21:48 +0530

    Handled flight edges

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -132,3 +132,31 @@ function createGraph(V, E) {
 [5,6,2], [2,5,4], [2,3,7], [3,5,14], [3,4,9], [4,5,10]];
   
   let graph = createGraph(V, E);
+
+  //Handling Flight edges
+ let plane = 0;
+ let p1 = -1, p2 = -1;
+
+ for(let pos in data['edges']) {
+   let edge = data['edges'][pos];
+
+   if(edge['type'] === 1) {
+     let to = edge['to'];
+     let from = edge['from'];
+     let wght = parseInt(edge['label']);
+
+     if(dist1[to][0] + wght+ dist2[from][0] < mn_dist) {
+       plane = wght;
+       p1 = to;
+       p2 = from;mn_dist = dist1[to][0] + dist2[from][0];
+     }
+
+     if(dist[to][0] + wght + dist[from][0]) {
+       plane = wght;
+       p2 = to;
+       p1 = from;
+       mn_dist = dist2[to][0] + wght + dost[from][0];
+     }
+   }
+ }
+
