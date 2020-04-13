e46a7e0c7517f95b62636e760aff04961c51ba21
Samarth Jain <jain.samarth2907@gmail.com>
2020-04-13 23:40:57 +0530

    Initial commit

---
+++ Cashfolow-Minimisation/index.js
@@ -0,0 +1,11 @@
+// Creating random data
+
+function createData() {
+  const size = Math.floor(Math.random() * 8) + 2;
+
+  // Adding people that represents nodes in a graph
+  let nodes = [];
+  for (let i = 0; i <= size; i++) {
+    nodes.push({});
+  }
+}
